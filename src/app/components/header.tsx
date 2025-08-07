9bdecd68105d3ffc345ac665df93851a97c8fac1
zaki <z.goumri@esi-sba.dz>
2025-08-07 09:03:59 +0100

    feat(background):implement the background

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,17 +1,29 @@
 import Image from "next/image";
 
+const routes = [
+  { label: "About me", route: "" },
+  { label: "Projects", route: "" },
+  { label: "Contact me", route: "" },
+];
+
 const Header = () => {
   return (
     <nav className="px-24 py-10">
-      <aside>
+      <aside className="flex gap-10 items-center">
         <Image
           src="/assets/figma.png"
           alt="figma logo"
           width={50}
           height={100}
         />
+        <p className="font-pangolin text-[#621c24] font-normal text-3xl">
+          Meriem is Here !
+        </p>
       </aside>
-      <aside></aside>
+      <aside>
+      {/* TODO implement routes */}
+{routes.map((route)=>  )} 
+      </aside>
     </nav>
   );
 };
